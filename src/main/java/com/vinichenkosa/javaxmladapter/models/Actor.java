063f4cad8fd22b2dfabe3d39fd32399f3ab0854e
Sergey Vinichenko <vinichenko@imp-m.ru>
2014-02-07 17:04:42 +0400

    Actor adapter added

--- src/main/java/com/vinichenkosa/javaxmladapter/models/Actor.java
+++ src/main/java/com/vinichenkosa/javaxmladapter/models/Actor.java
@@ -1,10 +1,13 @@
 package com.vinichenkosa.javaxmladapter.models;
 
+import com.vinichenkosa.javaxmladapter.adapters.ActorAdapter;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlAccessorType(XmlAccessType.NONE)
+@XmlJavaTypeAdapter(ActorAdapter.class)
 public class Actor {
     @XmlElement(name = "name")
     private String name;
@@ -12,6 +15,7 @@ public class Actor {
     private String surname;
     @XmlElement(name = "character")
     private String character;
+    private String fullName;
 
     public String getName() {
         return name;
@@ -37,9 +41,17 @@ public class Actor {
         this.character = character;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
     @Override
     public String toString() {
-        return "Actor{" + "name=" + name + ", surname=" + surname + 
-        ", character=" + character + '}';
+        return "Actor{" + "name=" + name + ", surname=" + surname + ", character=" + character + ", fullName=" + fullName + '}';
     }
+
 }
